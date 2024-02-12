5d00b092629866a5873ea91c67e06ffbd88f8bb6
techno-tanoC <develop.tanoc@gmail.com>
2024-02-13 00:39:52 +0900

    Add the api bin

---
+++ src/bin/api.rs
@@ -0,0 +1,15 @@
+use std::{env, net::SocketAddr};
+
+use anyhow::Result;
+use stockin_api::App;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let database_url = env::var("DATABASE_URL")?;
+    let port = env::var("PORT").unwrap_or("3000".to_string()).parse()?;
+    let app = App::new_app(&database_url).await?;
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = tokio::net::TcpListener::bind(addr).await?;
+    axum::serve(listener, app).await?;
+    Ok(())
+}
