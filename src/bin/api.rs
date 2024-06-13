871f0a65defd660ac8f9a4b87d190c30ecedbac8
techno-tanoC <develop.tanoc@gmail.com>
2024-06-13 19:42:37 +0900

    Install tracing, tracing-subscribe and tracing-stackdriver

--- src/bin/api.rs
+++ src/bin/api.rs
@@ -2,15 +2,25 @@ use std::{env, net::SocketAddr};
 
 use anyhow::Result;
 use stockin_api::App;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let stackdriver = tracing_stackdriver::layer();
+    tracing_subscriber::registry()
+        .with(stackdriver)
+        .with(EnvFilter::from_default_env())
+        .init();
+
     let database_url = env::var("DATABASE_URL")?;
     let port = env::var("PORT").unwrap_or("3000".to_string()).parse()?;
     let token = env::var("BEARER_TOKEN")?;
-    let app = App::new_app(&database_url, &token).await?;
+
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
     let listener = tokio::net::TcpListener::bind(addr).await?;
+
+    let app = App::new_app(&database_url, &token).await?;
+
     axum::serve(listener, app).await?;
     Ok(())
 }
