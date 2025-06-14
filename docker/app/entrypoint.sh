b9efd6ebce70a84ed7593461fe5dd3262700259c
techno-tanoC <develop.tanoc@gmail.com>
2025-06-14 19:30:07 +0900

    Update sqlite3def

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -3,5 +3,5 @@ set -eu
 
 mkdir -p db
 litestream restore -config ./litestream.yaml -if-db-not-exists -if-replica-exists db/database.sqlite3
-sqlite3def -f schema.sql db/database.sqlite3
+sqlite3def -f schema.sql --enable-drop db/database.sqlite3
 exec litestream replicate -config ./litestream.yaml -exec "./api"
